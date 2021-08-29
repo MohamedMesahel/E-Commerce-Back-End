783133d8afb19dc09acb30da3afe2a1e247d6acf
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-08-29 14:39:20 -0700

    reinstall package

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -10,8 +10,5 @@ const sequelize = process.env.JAWSDB_URL
       decimalNumbers: true,
     },
   });
-// const Sequelize = require('sequelize');
-
-
 
 module.exports = sequelize;
