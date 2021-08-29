e68254b88f677b575dbaa763a4612812f7d4b5f9
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-08-29 09:41:33 -0700

    Category api

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,61 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: [ 'id', 'product_name', 'price', 'category_id'],
+      },
+    ],
+  })
+})
+.then((dbCategoryData) => res.json(dbCategoryData))
+.catch((err) => {
+  console.log(err);
+  res.status(500).json(err)
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: [ 'id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res
+      .status(404)
+      .json({ message: 'No category data found for this category'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
