0dd1fae123c7d6b6a66e60d33ebd941d996bf60f
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-08-28 18:15:48 -0700

    product and product tag models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,37 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
